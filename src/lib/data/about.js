7b438a9c134deeeabb4a036660ec385a1943aff4
nosovandrew <andrewnosov@outlook.com>
2024-12-24 19:40:46 +0500

    ADD downloadable cv file

--- src/lib/data/about.js
+++ src/lib/data/about.js
@@ -1,6 +1,7 @@
 export const LINKS_COLORS = {
     contact: 'rgba(248, 229, 89, 1)',
     socialNetwork: 'rgba(134, 74, 249, 1)',
+    cv: 'rgba(243, 66, 19, 1)',
 };
 
 export const LINKS = [
@@ -18,5 +19,10 @@ export const LINKS = [
         title: 'Дипрофайл',
         url: 'https://dprofile.ru/nosov',
         color: LINKS_COLORS.socialNetwork,
+    },
+    {
+        title: 'Резюме.pdf',
+        url: `${import.meta.env.VITE_BASE_URL ?? 'http://localhost:5173'}/download/nosov-frontend-developer-cv.pdf`,
+        color: LINKS_COLORS.cv,
     },
 ]
